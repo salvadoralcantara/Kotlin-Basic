4b32dad5a2da491a6fc1e62ce7c820909739eb9c
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-02-03 15:18:52 -0600

    small basic practice of FrameLayout

---
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/Excercise1Activity.kt
@@ -0,0 +1,20 @@
+package com.CorporativeSA.miprimeraapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Excercise1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_excercise1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
