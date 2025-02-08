c0df11eb20459fb0bed54c4014d83968b66e8e57
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-02-08 15:22:23 -0600

    pequeña practica con LinearLayout

---
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/Exercise2Activity.kt
@@ -0,0 +1,20 @@
+package com.CorporativeSA.miprimeraapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Exercise2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_exercise2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
