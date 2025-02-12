30bd205f4190d4f61c7b109262f241b3016e9db7
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-02-12 10:04:01 -0600

    Uso de  ConstraintLayout para login basico

--- MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/Exercise1Activity.kt
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/Exercise1Activity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Excercise1Activity : AppCompatActivity() {
+class Exercise1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/Exercise3Activity.kt
@@ -0,0 +1,20 @@
+package com.CorporativeSA.miprimeraapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Exercise3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_exercise3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
