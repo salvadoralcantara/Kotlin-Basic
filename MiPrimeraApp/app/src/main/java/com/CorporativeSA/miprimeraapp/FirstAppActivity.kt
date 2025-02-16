e40ba4443b105e825455e9d1b6411b9272a527aa
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-02-16 10:46:52 -0600

    modificacion de activity principal

--- MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/FirstAppActivity.kt
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/FirstAppActivity.kt
@@ -1,8 +1,11 @@
 package com.CorporativeSA.miprimeraapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +14,13 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnFirstApp = findViewById<AppCompatButton>(R.id.btnFirstApp)
+        val etFirstApp = findViewById<AppCompatEditText>(R.id.etFirstApp)
+
+        btnFirstApp.setOnClickListener{
+            Log.i("FirstAppActivity", "Hola ${etFirstApp.text.toString()}")
+
         }
     }
-}
+
+    }
