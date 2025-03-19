c8c799480ae0944472108bcde4ebfa99e8b3863f
salvadoralcantara <salvadoralcantara626@gmail.com>
2025-03-19 10:04:47 -0600

    Agragando activity para calculador de IMC

--- MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/MenuActivity.kt
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/MenuActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.CorporativeSA.miprimeraapp.imcCalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +16,17 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnSaludo = findViewById<AppCompatButton>(R.id.btnSaludo)
+        var btnImcApp = findViewById<AppCompatButton>(R.id.btnImcApp)
         btnSaludo.setOnClickListener { navigeteToSaludoActivity() }
+        btnImcApp.setOnClickListener{navigateToImcApp()}
     }
 
-    fun navigeteToSaludoActivity() {
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigeteToSaludoActivity() {
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
---
+++ MiPrimeraApp/app/src/main/java/com/CorporativeSA/miprimeraapp/imcCalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,21 @@
+package com.CorporativeSA.miprimeraapp.imcCalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.CorporativeSA.miprimeraapp.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_imc_calculator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
